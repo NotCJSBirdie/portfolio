78ea6250fac1714d7102f2589123d3ed59e0cd64
Carl Serquiña <serquinajhunlo@gmail.com>
2025-08-15 05:23:14 +0800

    edited the issuer

--- app/data/certifications.js
+++ app/data/certifications.js
@@ -1,7 +1,7 @@
 export const certifications = [
   {
     name: 'IBM Back-End Development Professional Certificate',
-    issuer: 'Coursera (Authorized by IBM)',
+    issuer: 'IBM',
     date: 'August 2025',
     description:
       'Skilled in scaling techniques and methodologies including Docker, Kubernetes, microservices, and serverless functions. Proficient in writing back-end applications using server-side Python, version control, and package managers. Experienced in applying CI/CD and monitoring methodologies to streamline automation throughout the software development life cycle.',
